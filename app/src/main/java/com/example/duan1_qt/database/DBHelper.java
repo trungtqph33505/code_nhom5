28de65195995dcbd53a62164fa2d21ba2b00fd0e
trungtqph33505 <trungtqph33505>
2023-11-18 11:58:05 +0700

    test

--- app/src/main/java/com/example/duan1_qt/database/DBHelper.java
+++ app/src/main/java/com/example/duan1_qt/database/DBHelper.java
@@ -20,53 +20,14 @@ public DBHelper(@Nullable Context context){
 }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-    String User = "CREATE TABLE User (" +
-                "ID TEXT PRIMARY KEY AUTOINCREMENT, " +
-                "taiKhoan TEXT NOT NULL, " +
-                "matKhau TEXT NOT NULL )";
-    sqLiteDatabase.execSQL(User);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-    if ( i < i1 ){
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS User");
-        onCreate(sqLiteDatabase);
-    }
+
     }
 
-    public boolean Insert(String taiKhoan, String matKhau){
-    SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-
-        contentValues.put("taiKhoan",taiKhoan );
-        contentValues.put("matKhau",matKhau);
-        long result = sqLiteDatabase.insert("User", null,contentValues);
-        if (result == -1 ){
-            return false;
-        }else {
-            return true;
-        }
     }
 
-    public Boolean CheckUsername (String taiKhoan){
-    SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM User WHERE taiKhoan=?", new String[]{taiKhoan} );
-        if (cursor.getCount() > 0){
-            return false;
-        }else {
-            return true;
-        }
-    }
 
-    public Boolean CheckLogin (String taiKhoan , String matKhau){
-        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM User WHERE taiKhoan=? AND matKhau=?", new String[]{taiKhoan, matKhau} );
-        if (cursor.getCount() > 0){
-            return false;
-        }else {
-            return true;
-        }
-    }
-
-}
